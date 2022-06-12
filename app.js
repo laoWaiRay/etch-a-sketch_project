9ae07bbcb0d2dbffbda8e060393e05390a96c639
Ray <raymondly96@gmail.com>
2022-06-12 13:33:00 -0700

    Add grid and grid reset btn

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
 const grid = document.querySelector('.grid');
-const tile = document.createElement('div');
+const body = document.querySelector('body');
+const btnReset = document.querySelector('.btnReset');
+
+const makeGrid = function(gridSize){
+    for(let i=0; i<gridSize**2; i++){
+        grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+        const tile = document.createElement('div');
+        tile.classList.add('square');
+        grid.append(tile);
+    }
+}
+
+let gridSize = prompt('Enter a grid size:');
+while(gridSize>100){
+    gridSize = prompt('Please enter a grid size less than 100:')
+}
+
+makeGrid(gridSize);
+
+
+btnReset.addEventListener('click', () => {
+    let gridSize = prompt('Please enter a grid size less than 100:')
+    if(gridSize <= 100){
+        makeGrid(gridSize)
+    }
+    while(gridSize>100){
+        gridSize = prompt('Please enter a grid size less than 100:')
+        if(gridSize <= 100){
+            makeGrid(gridSize)
+        }
+    }
+})
+
